ad0d92c287b3632811271c7b9c1c04bc2b322128
Liam Mercado <liam.mercado@mymail.champlain.edu>
2023-10-06 18:44:34 -0400

    Committing core/lm folder.

---
+++ enc_temp_folder/6fcf56ebc5ef8bed52ef2e47d1326d5/main.cpp
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+#include <ew/ewMath/ewMath.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <lm/shader.h> 
+
+unsigned int createVAO(float* vertexData, int numVertices);
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+const int SCREEN_WIDTH = 1080;
+const int SCREEN_HEIGHT = 720;
+
+/*
+float vertices[9] = {
+	//x   //y  //z   
+	-0.5, -0.5, 0.0, 
+	 0.5, -0.5, 0.0,
+	 0.0,  0.5, 0.0 
+};
+*/
+
+float vertices[18] = {
+	//x   //y  //z
+	//Triangle 1 
+	-0.5, -0.5, 0.0,
+	 0.5, -0.5, 0.0,
+	 0.5,  0.5, 0.0,
+	//Triangle 2
+	 0.5, -0.5, 0.0,
+	-0.5,  0.5, 0.0,
+	-0.5, -0.5, 0.0
+};
+
+float triangleColor[3] = { 1.0f, 0.5f, 0.0f };
+float triangleBrightness = 1.0f;
+bool showImGUIDemoWindow = true;
+
+int main() {
+
+
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return 1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return 1;
+	}
+
+	//Initialize ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	unsigned int vao = createVAO(vertices, 6);
+
+	lm::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
+	shader.use();
+
+	glBindVertexArray(vao);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Set uniforms
+		shader.setVec3("_Color", triangleColor[0], triangleColor[1], triangleColor[2]);
+		shader.setFloat("_Brightness", triangleBrightness);
+
+		//Wireframe
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//Shaded
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//Render UI
+		{
+			ImGui_ImplGlfw_NewFrame();
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui::NewFrame();
+
+			ImGui::Begin("Settings");
+			ImGui::Checkbox("Show Demo Window", &showImGUIDemoWindow);
+			ImGui::ColorEdit3("Color", triangleColor);
+			ImGui::SliderFloat("Brightness", &triangleBrightness, 0.0f, 1.0f);
+			ImGui::End();
+			if (showImGUIDemoWindow) {
+				ImGui::ShowDemoWindow(&showImGUIDemoWindow);
+			}
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		}
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
+
+unsigned int createVAO(float* vertexData, int numVertices) {
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//Define a new buffer id
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numVertices * 7, vertexData, GL_STATIC_DRAW);
+
+	//Position attribute
+	glVertexAttribPointer(0, 7, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	return vao;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
