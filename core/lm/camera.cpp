4040391d8537e9e8e329653b1c8cc80a930991c0
Liam Mercado <liam.mercado@mymail.champlain.edu>
2023-10-15 19:52:51 -0400

    Current progress on Assignment 5: Cameras

---
+++ core/lm/camera.cpp
@@ -0,0 +1,25 @@
+#include "camera.h"
+#include "transformations.h"
+#include "../ew/ewMath/vec3.h"
+#include "../ew/ewMath/mat4.h"
+
+namespace lm {
+	//World->View
+	ew::Mat4 Camera::ViewMatrix()
+	{
+		return LookAt(position, target, ew::Vec3(0, 1, 0));
+	}
+
+	//View->Clip
+	ew::Mat4 Camera::ProjectionMatrix()
+	{
+		if (orthographic)
+		{
+			return Orthographic(orthoSize, aspectRatio, nearPlane, farPlane);
+		}
+		else
+		{
+			return Perspective(fov, aspectRatio, nearPlane, farPlane);
+		}
+	}
+}
--- core/lm/transformations.h
+++ core/lm/transformations.h
@@ -1,7 +1,9 @@
 #pragma once
+#include "../ew/ewMath/ewMath.h"
 #include "../ew/ewMath/mat4.h"
 #include "../ew/ewMath/vec3.h"
 #include <cmath>
+#include <vector>
 
 namespace lm {
 
@@ -25,6 +27,7 @@ namespace lm {
 	};
 	//Rotation around X axis (pitch) in radians
 	inline ew::Mat4 RotateX(float rad) {
+		rad = ew::Radians(rad);
 		return ew::Mat4(
 			1, 0			, 0				, 0,
 			0, std::cos(rad), -std::sin(rad), 0,
@@ -34,6 +37,7 @@ namespace lm {
 	};
 	//Rotation around Y axis (yaw) in radians
 	inline ew::Mat4 RotateY(float rad) {
+		rad = ew::Radians(rad);
 		return ew::Mat4(
 			std::cos(rad) , 0, std::sin(rad), 0,
 			0			  , 1, 0			, 0,
@@ -43,6 +47,7 @@ namespace lm {
 	};
 	//Rotation around Z axis (roll) in radians
 	inline ew::Mat4 RotateZ(float rad) {
+		rad = ew::Radians(rad);
 		return ew::Mat4(
 			std::cos(rad), -std::sin(rad), 0, 0,
 			std::sin(rad), std::cos(rad) , 0, 0,
@@ -67,5 +72,71 @@ namespace lm {
 		ew::Mat4 getModelMatrix() const {
 			return Translate(position) * (RotateY(rotation.y) * RotateX(rotation.x) * RotateZ(rotation.z)) * Scale(scale);
 		}
+	};
+
+	//Creates a right handed view space
+	//eye = eye (camera) position
+	//target = position to look at
+	//up = up axis, usually(0,1,0)
+	inline ew::Mat4 LookAt(ew::Vec3 eye, ew::Vec3 target, ew::Vec3 up) {
+
+		ew::Vec3 f = ew::Normalize(target - eye);
+		ew::Vec3 r = ew::Normalize(ew::Cross(f, up));
+		ew::Vec3 u = ew::Cross(f, r);
+
+		ew::Mat4 R = ew::Mat4(
+			r.x, r.y, r.z, 0,
+			u.x, u.y, u.z, 0,
+			f.x, f.y, f.z, 0,
+			0  , 0  , 0  , 1
+		);
+
+		ew::Mat4 T = ew::Mat4(
+			1, 0, 0, -up.x,
+			0, 1, 0, -up.y,
+			0, 0, 1, -up.z,
+			0, 0, 0, 1
+		);
+
+		return R * T;
+
+		/*
+		return ew::Mat4(
+			r.x, r.y, r.z, -(ew::Cross(r, up)),
+			u.x, u.y, u.z, -(ew::Cross(u, up)),
+			f.x, f.y, f.z, -(ew::Cross(f, up)),
+			0  , 0  , 0  , 1
+		);
+		*/
+
+			//use ew::Cross for cross product!
+	};
+
+	//Orthographic projection
+	inline ew::Mat4 Orthographic(float height, float aspect, float near, float far) {
+
+		float width = aspect * height;
+		float r = width / 2;	// Right Bounds
+		float l = -r;			// Left Bounds
+		float t = height / 2;	// Top Bounds
+		float b = -t;			// Bottom Bounds
+
+		return ew::Mat4(
+			2 / (r - l), 0			, 0				  , -((r + l) / (r - l)),
+			0		   , 2 / (t - b), 0				  , -((t + b) / (t - b)),
+			0		   , 0			, 2 / (far - near), -((far + near) / (far - near)),
+			0		   , 0			, 0				  , 1
+		);
+	};
+
+	//Perspective projection
+	//fov = vertical aspect ratio (radians)
+	inline ew::Mat4 Perspective(float fov, float aspect, float near, float far) {
+		return ew::Mat4(
+			1 / (std::tan(fov / 2) * aspect), 0						 , 0														   , 0,
+			0							    , 1 / (std::tan(fov / 2)), 0														   , 0,
+			0								, 0						 , (near + far) / (near - far)								   , (2 * far * near) / (near - far),
+			0								, 0						 , -1														   , 0
+		);
 	};
 }
