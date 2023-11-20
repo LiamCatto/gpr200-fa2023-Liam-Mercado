6ee90e96aa59c170f47acdcf4fefd83334e7b2a4
Liam Mercado <liam.mercado@mymail.champlain.edu>
2023-11-19 22:00:14 -0500

    Finished assignment 7

--- assignments/assignment7_lighting/assets/defaultLit.frag
+++ assignments/assignment7_lighting/assets/defaultLit.frag
@@ -11,13 +11,78 @@ struct Light
 {
 	vec3 position;
 	vec3 color;
+	int enable;
 };
+
 #define MAX_LIGHTS 4
 uniform Light _Lights[MAX_LIGHTS];
 uniform sampler2D _Texture;
 
+uniform vec3 _ViewPos;
+
+uniform float _lightIntensity;		// light intensity
+uniform vec3 _aColor;				// ambient light color
+uniform float _aCoef;				// ambient light intensity
+uniform float _shininess;			// shininess
+
+uniform bool _Enable1; // unused
+uniform bool _Enable2; // unused
+uniform bool _Enable3; // unused
+uniform bool _Enable4; // unused
+
+// Function prototypes
+
+float dotProduct(vec3 a, vec3 b);
+float ambient();
+float diffuse(Light light);
+float specular(Light light);
+float calcLight(Light light);
+bool enableLights[4] = { _Enable1, _Enable2, _Enable3, _Enable4 }; // unused
+
 void main(){
+	float light = 0.0;
+	vec4 color = vec4(0, 0, 0, 1);
+
+	for (int i = 0; i < MAX_LIGHTS; i++)
+	{
+		if (_Lights[i].enable == 1)
+		{
+			color += vec4(_Lights[i].color, 1) * (calcLight(_Lights[i]) / 4);
+		}
+	}
+
+	FragColor = texture(_Texture,fs_in.UV) + color;
+}
+
+float dotProduct(vec3 a, vec3 b)
+{
+	return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+float ambient()
+{
+	return _aColor * _aCoef;
+}
+
+float diffuse(Light light)
+{
 	vec3 normal = normalize(fs_in.WorldNormal);
-	//vec2 lighting = _Texture * max(dot(normal, fs_in.WorldPosition), 0);
-	FragColor = texture(_Texture,fs_in.UV) * max(dot(normal, fs_in.WorldPosition), 0);
+	vec3 incidence = normalize(light.position - fs_in.WorldPosition);
+	return _lightIntensity * max(dotProduct(normal, incidence), 0);
+}
+
+float specular(Light light)
+{
+	vec3 normal = normalize(fs_in.WorldNormal);
+	vec3 incidence = normalize(light.position - fs_in.WorldPosition);
+	vec3 viewDirection = normalize(_ViewPos - fs_in.WorldPosition);
+
+	vec3 halfVector = normalize(incidence + viewDirection);
+	
+	return _lightIntensity * pow(max(dotProduct(halfVector, normal), 0), _shininess);
+}
+
+float calcLight(Light light)
+{
+	return ambient() + diffuse(light) + specular(light);
 }
--- assignments/assignment7_lighting/main.cpp
+++ assignments/assignment7_lighting/main.cpp
@@ -30,7 +30,10 @@ ew::CameraController cameraController;
 struct Light {
 	ew::Vec3 position = ew::Vec3(0.0f, 2.0f, 0.0f); //World space
 	ew::Vec3 color = ew::Vec3(1.0f, 0.0f, 0.0f); //RGB
+	int enable = 1;
 };
+
+void resetLight(Light lights[], int light);
 
 struct Material {
 	float ambientK; //Ambient coefficient (0-1)
@@ -90,6 +93,45 @@ int main() {
 	planeTransform.position = ew::Vec3(0, -1.0, 0);
 	sphereTransform.position = ew::Vec3(-1.5f, 0.0f, 0.0f);
 	cylinderTransform.position = ew::Vec3(1.5f, 0.0f, 0.0f);
+	
+	Light lights[4];
+	bool enableLight_1 = true;
+	bool enableLight_2 = true;
+	bool enableLight_3 = true;
+	bool enableLight_4 = true;
+	bool enableLights[4] = { true, true, true, true };
+	float lightIntensity = 0.5;					// light intensity
+	ew::Vec3 aColor = ew::Vec3(0.5, 0.5, 0.5);	// ambient light color
+	float aCoef = 0.1;							// ambient light intensity
+	float shininess = 10.0;						// shininess
+	int numLights = 4;
+
+	for (int i = 0; i < 4; i++)
+	{
+		switch (i)
+		{
+			case 0:
+				lights[i].position = ew::Vec3(-2.0f, 1.0f, -2.0f);
+				lights[i].color = ew::Vec3(1.0f, 0.0f, 0.0f);
+				lights[i].enable = 1;
+				break;
+			case 1:
+				lights[i].position = ew::Vec3(2.0f, 1.0f, -2.0f);
+				lights[i].color = ew::Vec3(0.0f, 1.0f, 0.0f);
+				lights[i].enable = 1;
+				break;
+			case 2:
+				lights[i].position = ew::Vec3(2.0f, 1.0f, 2.0f);
+				lights[i].color = ew::Vec3(0.0f, 0.0f, 1.0f);
+				lights[i].enable = 1;
+				break;
+			case 3:
+				lights[i].position = ew::Vec3(-2.0f, 1.0f, 2.0f);
+				lights[i].color = ew::Vec3(0.5f, 0.5f, 0.0f);
+				lights[i].enable = 1;
+				break;
+		}
+	}
 
 	resetCamera(camera,cameraController);
 
@@ -113,11 +155,22 @@ int main() {
 		shader.setInt("_Texture", 0);
 		shader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
 
-		Light lights[4];
-		shader.setVec3("_Lights[0].position", lights[0].position);
-		shader.setVec3("_Lights[0].color", lights[0].color);
+		lights[0].enable = enableLight_1;
+		lights[1].enable = enableLight_2;
+		lights[2].enable = enableLight_3;
+		lights[3].enable = enableLight_4;
 
-		light_Shader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
+		for(int i = 0; i < 4; i++)
+		{
+			shader.setVec3("_Lights[" + std::to_string(i) + "].position", lights[i].position);
+			shader.setVec3("_Lights[" + std::to_string(i) + "].color", lights[i].color);
+			shader.setInt("_Lights[" + std::to_string(i) + "].enable", lights[i].enable);
+		}
+		shader.setVec3("_ViewPos", camera.position);
+		shader.setVec3("_aColor", aColor);
+		shader.setFloat("_lightIntensity", lightIntensity);
+		shader.setFloat("_aCoef", aCoef);
+		shader.setFloat("_shininess", shininess);
 
 		//Draw shapes
 		shader.setMat4("_Model", cubeTransform.getModelMatrix());
@@ -137,15 +190,50 @@ int main() {
 		light_Shader.use();
 		light_Shader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
 
+		/*
+		switch (numLights)
+		{
+		case 0:
+			enableLight_1 = false;
+			enableLight_2 = false;
+			enableLight_3 = false;
+			enableLight_4 = false;
+		case 1:
+			enableLight_1 = true;
+			enableLight_2 = false;
+			enableLight_3 = false;
+			enableLight_4 = false;
+		case 2:
+			enableLight_1 = true;
+			enableLight_2 = true;
+			enableLight_3 = false;
+			enableLight_4 = false;
+		case 3:
+			enableLight_1 = true;
+			enableLight_2 = true;
+			enableLight_3 = true;
+			enableLight_4 = false;
+		case 4:
+			enableLight_1 = true;
+			enableLight_2 = true;
+			enableLight_3 = true;
+			enableLight_4 = true;
+		}*/
+
+		enableLights[0] = enableLight_1;
+		enableLights[1] = enableLight_2;
+		enableLights[2] = enableLight_3;
+		enableLights[3] = enableLight_4;
+
 		for (int i = 0; i < 4; i++)
 		{
-			lights[i].position = ew::Vec3(0.0f, 2.0f, 0.0f);
-			lights[i].color = ew::Vec3(1.0f, 0.0f, 0.0f);
-			lightTransform.position = lights[i].position + ew::Vec3(i, 0.0f, 0.0f);
-			//lightTransform.position = lights[i].position;
-			light_Shader.setMat4("_Model", lightTransform.getModelMatrix());
-			light_Shader.setVec3("_Color", lights[i].color);
-			lightMesh.draw();
+			if (enableLights[i])
+			{
+				lightTransform.position = lights[i].position;
+				light_Shader.setMat4("_Model", lightTransform.getModelMatrix());
+				light_Shader.setVec3("_Color", lights[i].color);
+				lightMesh.draw();
+			}
 		}
 
 		//Render UI
@@ -171,6 +259,67 @@ int main() {
 				ImGui::DragFloat("Sprint Speed", &cameraController.sprintMoveSpeed, 0.1f);
 				if (ImGui::Button("Reset")) {
 					resetCamera(camera, cameraController);
+				}
+			}
+			if (ImGui::CollapsingHeader("Lights"))
+			{
+				if (ImGui::CollapsingHeader("Light 1"))
+				{
+					ImGui::Checkbox("Enable Light##1", &enableLight_1);
+					ImGui::DragFloat3("Position##1", &lights[0].position.x, 0.1f);
+					ImGui::DragFloat3("Color##1", &lights[0].color.x, 0.01f, 0.0, 1.0);
+					if (ImGui::Button("Reset##1")) {
+						resetLight(lights, 0);
+						enableLight_1 = true;
+					}
+				}
+				if (ImGui::CollapsingHeader("Light 2"))
+				{
+					ImGui::Checkbox("Enable Light##2", &enableLight_2);
+					ImGui::DragFloat3("Position##2", &lights[1].position.x, 0.1f);
+					ImGui::DragFloat3("Color##2", &lights[1].color.x, 0.01f, 0.0, 1.0);
+					if (ImGui::Button("Reset##2")) {
+						resetLight(lights, 1);
+						enableLight_2 = true;
+					}
+				}
+				if (ImGui::CollapsingHeader("Light 3"))
+				{
+					ImGui::Checkbox("Enable Light##3", &enableLight_3);
+					ImGui::DragFloat3("Position##3", &lights[2].position.x, 0.1f);
+					ImGui::DragFloat3("Color##3", &lights[2].color.x, 0.01f, 0.0, 1.0);
+					if (ImGui::Button("Reset##3")) {
+						resetLight(lights, 2);
+						enableLight_3 = true;
+					}
+				}
+				if (ImGui::CollapsingHeader("Light 4"))
+				{
+					ImGui::Checkbox("Enable Light##4", &enableLight_4);
+					ImGui::DragFloat3("Position##4", &lights[3].position.x, 0.1f);
+					ImGui::DragFloat3("Color##4", &lights[3].color.x, 0.01f, 0.0, 1.0);
+					if (ImGui::Button("Reset##4")) {
+						resetLight(lights, 3);
+						enableLight_4 = true;
+					}
+				}
+
+				ImGui::DragFloat3("Ambient Color", &aColor.x, 0.05, 0, 1);
+				ImGui::DragFloat("Light Intensity", &lightIntensity, 0.01);
+				ImGui::DragFloat("Ambient Intensity", &aCoef, 0.05);
+				ImGui::DragFloat("Shininess", &shininess, 0.05);
+				//ImGui::SliderInt("Number of Lights", &numLights, 0, 4);
+				if (ImGui::Button("Reset Lights")) {
+					for (int i = 0; i < 4; i++)
+					{
+						resetLight(lights, i);
+						enableLight_1, enableLight_2, enableLight_3, enableLight_4 = true;
+					}
+
+					aColor = ew::Vec3(0.5, 0.5, 0.5);
+					lightIntensity = 0.5;
+					aCoef = 0.1;
+					shininess = 10.0;
 				}
 			}
 
@@ -206,4 +355,25 @@ void resetCamera(ew::Camera& camera, ew::CameraController& cameraController) {
 	cameraController.pitch = 0.0f;
 }
 
-
+void resetLight(Light lights[], int light)
+{
+	switch (light)
+	{
+	case 0:
+		lights[0].position = ew::Vec3(-2.0f, 2.0f, -2.0f);
+		lights[0].color = ew::Vec3(1.0f, 0.0f, 0.0f);
+		break;
+	case 1:
+		lights[1].position = ew::Vec3(2.0f, 2.0f, -2.0f);
+		lights[1].color = ew::Vec3(0.0f, 1.0f, 0.0f);
+		break;
+	case 2:
+		lights[2].position = ew::Vec3(2.0f, 2.0f, 2.0f);
+		lights[2].color = ew::Vec3(0.0f, 0.0f, 1.0f);
+		break;
+	case 3:
+		lights[3].position = ew::Vec3(-2.0f, 2.0f, 2.0f);
+		lights[3].color = ew::Vec3(1.0f, 1.0f, 1.0f);
+		break;
+	}
+}
